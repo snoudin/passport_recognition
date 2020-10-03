ebbadaf870b38a001112c25bc71a450232738644
Snoudin <denis.r.vasiuk@gmail.com>
2020-10-03 12:18:05 +1000

    series, number, full name, gender, receiving place TODO: remove file storaging after sebug, create .cfg with templates and template setup method

--- crop.py
+++ crop.py
@@ -2,7 +2,7 @@ from PIL import Image
 import opencv_borders, recognize
 
 
-opencv_borders.get_bw('image.jpg')
+opencv_borders.get_bw('scan.jpg')
 img = Image.open('trimap.png')
 left, u, r, d = 0, 0, 0, 0
 pixels = img.load()
@@ -15,19 +15,28 @@ while all([pixels[x - r - 1, j] == 0 for j in range(y)]):
     r += 1
 while all([pixels[j, y - d - 1] == 0for j in range(x)]):
     d += 1
-Image.open('image.jpg').crop((left, u, x - r, y - d)).save('main.png')
+Image.open('scan.jpg').crop((left, u, x - r, y - d)).save('main.png')
 
 passport = Image.open('main.png')
 x, y = passport.size
+# TODO: take sizes from .cfg file
 snum = passport.crop((11 * x // 13, 0, x, y // 2)).transpose(Image.ROTATE_90)
 snum.save('series_and_number.png')
-surname = passport.crop((4 * x // 10, y // 20 * 11, 16 * x // 20, y // 20 * 13))
+surname = passport.crop((4 * x // 10, y // 60 * 32, 17 * x // 20, y // 60 * 37))
 surname.save('surname.png')
 recieved_in = passport.crop((2 * x // 10, y // 18 * 2, 18 * x // 20, y // 9 * 2))
 recieved_in.save('recieved_in.png')
+name = passport.crop((4 * x // 10, y // 60 * 37, 17 * x // 20, y // 20 * 13))
+name.save('name.png')
+patronym = passport.crop((4 * x // 10, y // 20 * 13, 17 * x // 20, y // 60 * 42))
+patronym.save('patronym.png')
+gender = passport.crop((11 * x // 30, y // 60 * 42, 10 * x // 20, y // 60 * 44))
+gender.save('gender.png')
+
 # TODO: same manipulations
-
-
 print(recognize.get_snum('series_and_number.png'))
-print(recognize.get_surname('surname.png'))
 print(recognize.get_recieved_in('recieved_in.png'))
+print(recognize.get_name('surname.png'))
+print(recognize.get_name('name.png'))
+print(recognize.get_name('patronym.png'))
+print(recognize.get_gender('gender.png'))
--- recognize.py
+++ recognize.py
@@ -17,7 +17,7 @@ def get_snum(name):
             val += 3
         else:
             break
-    return line
+    return tuple([line[:2] + line[3:5], line[6:]])
 
 
 def get_recieved_in(name):
@@ -36,8 +36,15 @@ def get_recieved_in(name):
         else:
             break
     while not line.startswith('ОТДЕЛ'):
-        line.pop(0)
-    return line.lstrip('\n\t ').rstrip('\n\t ')
+        line = line[1:]
+    line = line.replace('\n', ' ')
+    line = line.replace('--', '-')
+    line = line.replace(' -', '')
+    line = line.replace('_', '')
+    line = line.replace('..', '')
+    line = line.replace(' .', '')
+    line = line.replace('  ', ' ')
+    return line.lstrip(',.:;@#!?[]{}_+= \n\t\x0c^*()|\\/').rstrip(',.:;@#!?[]{}_+= \n\t\x0c^*()|\\/')
 
 
 def more_contrast(img):
@@ -49,14 +56,52 @@ def more_contrast(img):
     return cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
 
 
-def get_surname(name):
+def get_name(name):
     img = cv2.imread(name, 1)
     img = more_contrast(img)
-    img = cv2.imread(name)
     pytesseract.pytesseract.tesseract_cmd = r'tesseract v5.0.0\tesseract.exe'
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    im_bw = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)[1]
+    im_bw = cv2.threshold(gray, 90, 255, cv2.THRESH_BINARY)[1]
     cv2.imwrite("bw.png", im_bw)
     surname = Image.open('bw.png')
     line = pytesseract.image_to_string(surname, lang='rus')
-    return line.lstrip('\n\t ').rstrip('\n\t ')
+    while line[0] in '-,.:;@#!?[]{}_+= \n\t\x0c^*()|\\/' or ord(line[0]) > 1200 or line[0].islower():
+        line = line[1:]
+    while line[-1] in '-,.:;@#!?[]{}_+= \n\t\x0c^*()|\\/' or ord(line[-1]) > 1200 or line[-1].islower():
+        line = line[:-1]
+    return line
+
+
+def get_gender(name):
+    img = cv2.imread(name, 1)
+    pytesseract.pytesseract.tesseract_cmd = r'tesseract v5.0.0\tesseract.exe'
+    val = 90
+    line = ''
+    while line not in ['МУЖ.', 'ЖЕН.']:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        im_bw = cv2.threshold(gray, val, 255, cv2.THRESH_BINARY)[1]
+        cv2.imwrite("bw.png", im_bw)
+        gender = Image.open('bw.png')
+        line = pytesseract.image_to_string(gender, lang='rus').lstrip('-,.:;@#!?[]{}_+= \n\t\x0c^*()|\\/').rstrip('-,:;@#!?[]{}_+= \n\t\x0c^*()|\\/')
+        if len(line) > 40 or len(line) < 12 or sum([1 for i in line if i.isdigit()]) != 10:
+            val += 3
+        else:
+            break
+    return line
+
+def get_date(name):
+    img = cv2.imread(name)
+    pytesseract.pytesseract.tesseract_cmd = r'tesseract v5.0.0\tesseract.exe'
+    val = 150
+    line = ''
+    while 1:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        im_bw = cv2.threshold(gray, val, 255, cv2.THRESH_BINARY)[1]
+        cv2.imwrite("bw.png", im_bw)
+        numbers = Image.open('bw.png')
+        line = pytesseract.image_to_string(numbers).lstrip('-,.:;@#!?[]{}_+= \n\t\x0c^*()|\\/').rstrip('-,:;@#!?[]{}_+= \n\t\x0c^*()|\\/')
+        if len(line) > 40 or len(line) < 10 or sum([1 for i in line if i.isdigit()]) != 8:
+            val += 3
+        else:
+            break
+    return line
